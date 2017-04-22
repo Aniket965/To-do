dfc0b9d4e4d2a92d0a349e6fc1321ee5e071bb8c
aniket sharma <aniket965.as@gmail.com>
2017-04-22 18:46:09 +0530

    added something

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     var handgone = false;
     setInterval(function () {
         $('.loading').fadeOut(1000);
+        $('#todolist').fadeIn(1000);
         if (handgone === false) {
             $('#welcome').fadeIn(1000);
             handgone = true;
@@ -10,22 +11,21 @@ $(document).ready(function () {
 
     }, 5000);
     $('#welcome').css("color", "red");
-    $('#todolist').html(markdown.toHTML(getText('https://github.com/Aniket965/fampool/blob/master/README.md')));});
+    $('#todolist').html(markdown.toHTML(getText('https://raw.githubusercontent.com/Aniket965/To-do/master/README.md')));
 
-//     $.get("https://github.com/Aniket965/fampool/blob/master/README.md?raw=true", function(data, status){
-//         alert("Data: " + data + "\nStatus: " + status);
+});
 
-// });
-
- function getText(myUrl){
-            var result = null;
-            $.ajax( { url: myUrl, 
-                      type: 'get', 
-                      dataType: 'html',
-                      async: false,
-                      success: function(data) { result = data; } 
-                    }
-            );
-            FileReady = true;
-            return result;
+function getText(myUrl) {
+    var result = null;
+    $.ajax({
+        url: myUrl,
+        type: 'get',
+        dataType: 'html',
+        async: false,
+        success: function (data) {
+            result = data;
         }
+    });
+    FileReady = true;
+    return result;
+}
