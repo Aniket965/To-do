053395935d328a1e48cfd29f8b2de50f3821bd37
aniket sharma <aniket965.as@gmail.com>
2017-04-22 18:09:56 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
-var handgone = false ;
+    var handgone = false;
     setInterval(function () {
         $('.loading').fadeOut(1000);
-        if(handgone === false) {
+        if (handgone === false) {
             $('#welcome').fadeIn(1000);
             handgone = true;
-           
+
         }
-    
-}, 5000);
- $('#welcome').css("color" , "red");
+
+    }, 5000);
+    $('#welcome').css("color", "red");
+    $('#todolist').html(markdown.toHTML('Hello.\n\n* This is markdown.\n* It is fun\n* Love it or leave it'));});
+
+    $.get("https://github.com/Aniket965/fampool/blob/master/README.md?raw=true", function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
 
 });
